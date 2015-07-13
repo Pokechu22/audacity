79d4e2464d234492f44d8d8a0b0d3c70afa08d76
Paul Licameli <paul.licameli@gmail.com>
2015-07-13 19:32:26 -0400

    TrackPanel no longer implements clip joining or cutline expansion/deletion...
    ... also implement ESC key for these and added status bar message for
    mouse-over

--- src/TrackPanel.h
+++ src/TrackPanel.h
@@ -21,7 +21,6 @@
 #include "widgets/NumericTextCtrl.h"
 
 #include "SelectedRegion.h"
-#include "WaveTrackLocation.h"
 
 #include "widgets/OverlayPanel.h"
 
@@ -257,8 +256,6 @@ class AUDACITY_DLL_API TrackPanel final : public OverlayPanel {
    virtual bool HandleLabelTrackClick(LabelTrack * lTrack, const wxRect &rect, wxMouseEvent & event);
    virtual void HandleGlyphDragRelease(LabelTrack * lTrack, wxMouseEvent & event);
    virtual void HandleTextDragRelease(LabelTrack * lTrack, wxMouseEvent & event);
-   virtual bool HandleTrackLocationMouseEvent(WaveTrack * track, const wxRect &rect, wxMouseEvent &event);
-   virtual bool IsOverCutline(WaveTrack * track, const wxRect &rect, const wxMouseEvent &event);
    virtual void HandleTrackSpecificMouseEvent(wxMouseEvent & event);
 
    virtual void ScrollDuringDrag();
@@ -336,7 +333,6 @@ protected:
 
    // AS: Cursor handling
    virtual bool SetCursorByActivity( );
-   virtual bool SetCursorForCutline(WaveTrack * track, const wxRect &rect, const wxMouseEvent &event);
    virtual void SetCursorAndTipWhenInLabel( Track * t, const wxMouseEvent &event, wxString &tip );
    virtual void SetCursorAndTipWhenInVResizeArea( bool blinked, wxString &tip );
    virtual void SetCursorAndTipWhenInLabelTrack( LabelTrack * pLT, const wxMouseEvent & event, wxString &tip );
@@ -603,8 +599,6 @@ protected:
 #endif
 
    Track *mCapturedTrack;
-   WaveTrackLocation mCapturedTrackLocation;
-   wxRect mCapturedTrackLocationRect;
    wxRect mCapturedRect;
 
    bool mRedrawAfterStop;
@@ -686,7 +680,6 @@ protected:
       IsGainSliding,
       IsPanSliding,
       IsMinimizing,
-      WasOverCutLine,
       IsPopping,
    };
 
