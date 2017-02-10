cd7727fd2a0b99dbf50f4581b1e4b7af1085fa5c
Pokechu22 <Pokechu022@gmail.com>
2017-02-10 13:33:18 -0800

    Draft part 1

--- src/NoteTrack.h
+++ src/NoteTrack.h
@@ -98,8 +98,8 @@ class AUDACITY_DLL_API NoteTrack final : public Track {
    bool Shift(double t) /* not override */;
 
 #ifdef EXPERIMENTAL_MIDI_OUT
-   float GetGain() const { return mGain; }
-   void SetGain(float gain) { mGain = gain; }
+   float GetVelocity() const { return mVelocity; }
+   void SetVelocity(float velocity) { mVelocity = velocity; }
 #endif
 
    double NearestBeatTime(double time, double *beat);
@@ -165,11 +165,6 @@ class AUDACITY_DLL_API NoteTrack final : public Track {
    void StartVScroll();
    void VScroll(int start, int end);
 
-#ifdef EXPERIMENTAL_MIDI_OUT
-   wxRect GetGainPlacementRect() const { return mGainPlacementRect; }
-   void SetGainPlacementRect(const wxRect &r) { mGainPlacementRect = r; }
-#endif
-
    bool HandleXMLTag(const wxChar *tag, const wxChar **attrs) override;
    XMLTagHandler *HandleXMLChild(const wxChar *tag) override;
    void WriteXML(XMLWriter &xmlFile) override;
@@ -202,7 +197,7 @@ class AUDACITY_DLL_API NoteTrack final : public Track {
    long mSerializationLength;
 
 #ifdef EXPERIMENTAL_MIDI_OUT
-   float mGain; // velocity offset
+   float mVelocity; // velocity offset
 #endif
 
    // mBottom is the Y offset of pitch 0 (normally off screen)
@@ -212,7 +207,6 @@ class AUDACITY_DLL_API NoteTrack final : public Track {
    int mPitchHeight;
    int mVisibleChannels; // bit set of visible channels
    int mLastMidiPosition;
-   wxRect mGainPlacementRect;
 };
 
 #endif // USE_MIDI
