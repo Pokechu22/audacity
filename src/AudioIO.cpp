Pokechu22/audacity

commit 67cec5ad83dd1947c52119eaf73fb1c7fba9d86d

Make many counts of tracks and channels unsigned...
... And in some places where a library uses signed types, assert that
the reported number is not negative.

What led me to this, is that there are many places where a size_t value for
an allocation is the product of a number of channels and some other number.

src/AudioIO.cpp
@@ -1948,7 +1948,7 @@ int AudioIO::StartStream(const WaveTrackArray &playbackTracks,
       {
          WaveTrack *vt = gAudioIO->mPlaybackTracks[i];
 
-         int chanCnt = 1;
+         unsigned chanCnt = 1;
          if (vt->GetLinked())
          {
             i++;
@@ -3634,7 +3634,7 @@ void AudioIO::FillBuffers()
          // Append captured samples to the end of the WaveTracks.
          // The WaveTracks have their own buffering for efficiency.
          AutoSaveFile blockFileLog;
-         int numChannels = mCaptureTracks.size();
+         auto numChannels = mCaptureTracks.size();
 
          for( i = 0; (int)i < numChannels; i++ )
          {
@@ -3871,9 +3871,8 @@ bool AudioIO::SetHasSolo(bool hasSolo)
 void AudioIO::FillMidiBuffers()
 {
    bool hasSolo = false;
-   int numPlaybackTracks = gAudioIO->mPlaybackTracks.size();
-   int t;
-   for(t = 0; t < numPlaybackTracks; t++ )
+   auto numPlaybackTracks = gAudioIO->mPlaybackTracks.size();
+   for(unsigned t = 0; t < numPlaybackTracks; t++ )
       if( gAudioIO->mPlaybackTracks[t]->GetSolo() ) {
          hasSolo = true;
          break;
@@ -4120,7 +4119,7 @@ void AudioIO::AILAProcess(double maxPeak) {
 
 static void DoSoftwarePlaythrough(const void *inputBuffer,
                                   sampleFormat inputFormat,
-                                  int inputChannels,
+                                  unsigned inputChannels,
                                   float *outputBuffer,
                                   int len)
 {
@@ -4151,9 +4150,9 @@ int audacityAudioCallback(const void *inputBuffer, void *outputBuffer,
 #endif
                           const PaStreamCallbackFlags WXUNUSED(statusFlags), void * WXUNUSED(userData) )
 {
-   int numPlaybackChannels = gAudioIO->mNumPlaybackChannels;
-   int numPlaybackTracks = gAudioIO->mPlaybackTracks.size();
-   int numCaptureChannels = gAudioIO->mNumCaptureChannels;
+   auto numPlaybackChannels = gAudioIO->mNumPlaybackChannels;
+   auto numPlaybackTracks = gAudioIO->mPlaybackTracks.size();
+   auto numCaptureChannels = gAudioIO->mNumCaptureChannels;
    int callbackReturn = paContinue;
    void *tempBuffer = alloca(framesPerBuffer*sizeof(float)*
                              MAX(numCaptureChannels,numPlaybackChannels));
@@ -4178,7 +4177,6 @@ int audacityAudioCallback(const void *inputBuffer, void *outputBuffer,
 #endif
 
    unsigned int i;
-   int t;
 
    /* Send data to recording VU meter if applicable */
 
@@ -4322,7 +4320,7 @@ int audacityAudioCallback(const void *inputBuffer, void *outputBuffer,
             gAudioIO->mWarpedTime = std::abs(gAudioIO->mWarpedTime);
 
             // Reset mixer positions and flush buffers for all tracks
-            for (i = 0; i < (unsigned int)numPlaybackTracks; i++)
+            for (i = 0; i < numPlaybackTracks; i++)
             {
                gAudioIO->mPlaybackMixers[i]->Reposition(gAudioIO->mTime);
                const auto toDiscard =
@@ -4347,8 +4345,8 @@ int audacityAudioCallback(const void *inputBuffer, void *outputBuffer,
             return paContinue;
          }
 
-         int numSolo = 0;
-         for( t = 0; t < numPlaybackTracks; t++ )
+         unsigned numSolo = 0;
+         for(unsigned t = 0; t < numPlaybackTracks; t++ )
             if( gAudioIO->mPlaybackTracks[t]->GetSolo() )
                numSolo++;
 #ifdef EXPERIMENTAL_MIDI_OUT
@@ -4372,7 +4370,7 @@ int audacityAudioCallback(const void *inputBuffer, void *outputBuffer,
          int group = 0;
          int chanCnt = 0;
          int maxLen = 0;
-         for (t = 0; t < numPlaybackTracks; t++)
+         for (unsigned t = 0; t < numPlaybackTracks; t++)
          {
             WaveTrack *vt = gAudioIO->mPlaybackTracks[t];
 
@@ -4573,7 +4571,7 @@ int audacityAudioCallback(const void *inputBuffer, void *outputBuffer,
       if( inputBuffer && (numCaptureChannels > 0) )
       {
          size_t len = framesPerBuffer;
-         for( t = 0; t < numCaptureChannels; t++) {
+         for(unsigned t = 0; t < numCaptureChannels; t++) {
             len = std::min( len,
                gAudioIO->mCaptureBuffers[t]->AvailForPut());
          }
@@ -4585,7 +4583,7 @@ int audacityAudioCallback(const void *inputBuffer, void *outputBuffer,
          }
 
          if (len > 0) {
-            for( t = 0; t < numCaptureChannels; t++) {
+            for(unsigned t = 0; t < numCaptureChannels; t++) {
 
                // dmazzoni:
                // Un-interleave.  Ugly special-case code required because the

commit e12ad644be1283496c2c20a5da7b5de3c27ce89b

Fix midi-related compile problems in AudioIO and NoteTrack
The compilation was broken in 67cec5a but fixed in ed8c7fd.  However, the warnings weren't fixed.  The same general sign-related changes from 67cec5a were applied, fixing several warnings.

This also changes the arguments for NoteTrack's label click handler, to match the ones found in TrackPanel.  That's needed to handle some other const-related changes.

Additionally, EXPERIMENTAL_MIDI_PLAYBACK was changed to EXPERIMENTAL_MIDI_OUT in the documentation, as that is the correct name.

src/AudioIO.cpp
@@ -28,8 +28,8 @@
   To highlight this deliniation, the file is divided into three parts
   based on what thread context each function is intended to run in.
 
-  \par EXPERIMENTAL_MIDI_PLAYBACK
-  If EXPERIMENTAL_MIDI_PLAYBACK is defined, this class also manages
+  \par EXPERIMENTAL_MIDI_OUT
+  If EXPERIMENTAL_MIDI_OUT is defined, this class also manages
   MIDI playback. The reason for putting MIDI here rather than in, say,
   class MidiIO, is that there is no high-level synchronization and
   transport architecture, so Audio and MIDI must be coupled in order
@@ -3902,7 +3902,7 @@ void AudioIO::FillMidiBuffers()
          hasSolo = true;
          break;
       }
-   int numMidiPlaybackTracks = gAudioIO->mMidiPlaybackTracks.size();
+   auto numMidiPlaybackTracks = gAudioIO->mMidiPlaybackTracks.size();
    for(unsigned t = 0; t < numMidiPlaybackTracks; t++ )
       if( gAudioIO->mMidiPlaybackTracks[t]->GetSolo() ) {
          hasSolo = true;
@@ -4375,7 +4375,7 @@ int audacityAudioCallback(const void *inputBuffer, void *outputBuffer,
             if( gAudioIO->mPlaybackTracks[t]->GetSolo() )
                numSolo++;
 #ifdef EXPERIMENTAL_MIDI_OUT
-         int numMidiPlaybackTracks = gAudioIO->mMidiPlaybackTracks.size();
+         auto numMidiPlaybackTracks = gAudioIO->mMidiPlaybackTracks.size();
          for( unsigned t = 0; t < numMidiPlaybackTracks; t++ )
             if( gAudioIO->mMidiPlaybackTracks[t]->GetSolo() )
                numSolo++;

src/NoteTrack.cpp
@@ -239,7 +239,7 @@ void NoteTrack::WarpAndTransposeNotes(double t0, double t1,
 
 
 
-int NoteTrack::DrawLabelControls(wxDC & dc, wxRect & r)
+int NoteTrack::DrawLabelControls(wxDC & dc, wxRect r)
 {
    int wid = 23;
    int ht = 16;
@@ -328,7 +328,7 @@ int NoteTrack::DrawLabelControls(wxDC & dc, wxRect & r)
    return box.GetBottom();
 }
 
-bool NoteTrack::LabelClick(wxRect & r, int mx, int my, bool right)
+bool NoteTrack::LabelClick(wxRect rect, int mx, int my, bool right)
 {
    int wid = 23;
    int ht = 16;

src/NoteTrack.h
@@ -73,8 +73,8 @@ class AUDACITY_DLL_API NoteTrack final : public Track {
    void WarpAndTransposeNotes(double t0, double t1,
                               const TimeWarper &warper, double semitones);
 
-   int DrawLabelControls(wxDC & dc, wxRect & r);
-   bool LabelClick(wxRect & r, int x, int y, bool right);
+   int DrawLabelControls(wxDC & dc, wxRect r);
+   bool LabelClick(wxRect rect, int x, int y, bool right);
 
    void SetSequence(std::unique_ptr<Alg_seq> &&seq);
    void SwapSequence(NoteTrack &other);